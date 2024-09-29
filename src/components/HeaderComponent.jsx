4a0cc98650d9d485214c3f846b14688893a86ab0
Yncy0 <paalayancy@gmail.com>
2024-09-29 20:56:03 +0800

    init: body components

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass, faBell, faUser} from '@fortawesome/free-solid-svg-icons'
-import Hamburger from 'hamburger-react'
 import MenuComponent from './navigation/MenuComponent'
 
 export default function HeaderComponent() {
