267a1942b87b3794c2a9fe93c8f68160b1ffbd5b
Yncy0 <paalayancy@gmail.com>
2024-10-01 21:29:57 +0800

    fix: flex layout

--- src/Root.jsx
+++ src/Root.jsx
@@ -6,14 +6,14 @@ import NavBarComponent from "./components/navigation/NavBarComponent";
 export default function Root() {
     return(
         <>
-            {/* <div className="root-div">
+            <div className="root-div">
                 <NavBarComponent/>
                 <div className="root-column">
                     <HeaderComponent/>
                     <Outlet/>     
                 </div>
-            </div> */}
-            <NavBarComponent/>
+            </div>
+            {/* <NavBarComponent/> */}
         </>
     )
 }
