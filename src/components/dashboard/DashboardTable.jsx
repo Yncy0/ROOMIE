5a2cb197fa1ae517a226f79c962e5e25778cc6e1
Yncy0 <paalayancy@gmail.com>
2024-10-03 18:12:23 +0800

    test: tailwind on dashboard table

--- src/components/dashboard/DashboardTable.jsx
+++ src/components/dashboard/DashboardTable.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react"
 export default function DashbaordTable() {
-    const [rooms, setRooms] = useState("St. Agustine Bldg.")
+    const [rooms, setRooms] = useState([]);
+    const [roomName, setRoomName] = useState("");
 
     return(
-        <div className="dashboard-table round-box">
-            <h1>Top Booked Rooms</h1>
-            <h3>Rooms</h3>
-            <h3>Times Booked</h3>
-            <h3>Cancelled</h3>
+        <div className="flex flex-col p-4 bg-white shadow-xl rounded-md">
+             <h1>Top Booked Rooms</h1>
+            <div className="flex flex-row justify-between">
+                <h3>Rooms</h3>
+                <h3>Times Booked</h3>
+                <h3>Cancelled</h3>
+            </div>
+            <ul>
+                {rooms.map((element, index) => {
+                    <li key={index}>
+                        <p>{element}</p>
+                    </li>
+                })}
+            </ul>
         </div>
     )
 }
