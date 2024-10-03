5102d3768590c0f343b39e84fc03b97354f50303
Yncy0 <paalayancy@gmail.com>
2024-10-03 18:18:18 +0800

    convert: ColumnCard into tailwind

--- src/components/dashboard/ColumnCard.jsx
+++ src/components/dashboard/ColumnCard.jsx
@@ -2,9 +2,8 @@
 
 export default function ColumnCard(props) {
     return(
-        <div className="column-card round-box">
-            <img src="src\assets\bg - icon1.png" alt="" />
-            <div className="column-text">
+        <div className="flex flex-col justify-between p-4 w-80 bg-white shadow-xl rounded-md">
+            <div className="flex flex-col text-right">
                 <span>{props.header}</span>
                 <h2>{props.stats}</h2>
             </div>
