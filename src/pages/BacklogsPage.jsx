2171abfd70cdca2c4bf850c2c5ff528ee2e6a584
Yncy0 <paalayancy@gmail.com>
2024-10-07 20:33:19 +0800

    add: backlogs layout

--- src/pages/BacklogsPage.jsx
+++ src/pages/BacklogsPage.jsx
@@ -1,7 +1,11 @@
+import { BacklogsTable } from "@/components/backlogs/BacklogsTable";
 
 
 export default function BacklogsPage() {
     return(
-        <h1>BACKLOGS</h1>
+        <>
+            <h1>BACKLOGS</h1>
+            <BacklogsTable />
+        </>
     )
 }
