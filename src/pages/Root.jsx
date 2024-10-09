7bd76af6239d0769e1b5cf5363c9d0f596a7a64c
Yncy0 <paalayancy@gmail.com>
2024-10-09 10:13:12 +0800

    fixed: navbar fixed position

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -6,7 +6,7 @@ import NavBarComponent from "../components/navigation/NavBarComponent";
 export default function Root() {
     return(
         <>
-            <div className="flex flex-row gap-4 m-4">
+            <div className="flex flex-row gap-4">
                 <NavBarComponent/>
                 <div className="flex flex-col gap-4">
                     <HeaderComponent/>
