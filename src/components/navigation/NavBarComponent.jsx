3b3c49fd789286359a3cb348772b6cb136ace855
Yncy0 <paalayancy@gmail.com>
2024-10-09 14:57:08 +0800

    fixed: finally, just a one liner code bugs

--- src/components/navigation/NavBarComponent.jsx
+++ src/components/navigation/NavBarComponent.jsx
@@ -11,34 +11,33 @@ export default function NavBarComponent() {
 
     const openSideBar = () => {
         setIsOpen(!isOpen)
-        console.log(isOpen)
     }
 
     return(
-            <div className="menu-container round-box p-6 gap-9 bg-white">
-                <div className="flex flex-row items-center gap-4">
-                    <button onClick={openSideBar}>
-                        <FontAwesomeIcon icon={faBars} />
-                    </button>
-                    <h1 className={`${isOpen ? "hidden" : "block"}`}>ROOMIE</h1>
-                </div>
-                <ul className="flex flex-col gap-12 pb-64">
-                    {Navigation.map((nav) => {
-                        const {Icon} = nav
-                        return(
-                            <li key={nav.title}>
-                                <Link 
-                                    to={`/${nav.title.toLowerCase()}`}
-                                    onClick={() => setIsOpen((prev) => !prev)}
-                                    className="flex flex-row items-center gap-4"
-                                >   
-                                    <FontAwesomeIcon icon={Icon} />
-                                    {!isOpen && <span >{nav.title}</span>}
-                                </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
+        <div className="menu-container round-box p-6 gap-9 bg-white">
+            <div className="flex flex-row items-center gap-4">
+                <button onClick={openSideBar}>
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+                <h1 className={!isOpen ? "block" : "hidden"}>ROOMIE</h1>
             </div>
+            <ul className="flex flex-col gap-12 pb-64">
+                {Navigation.map((nav) => {
+                    const {Icon} = nav
+                    return(
+                        <li key={nav.title}>
+                            <Link 
+                                to={`/${nav.title.toLowerCase()}`}
+                                // onClick={() => setIsOpen((prev) => !prev)}
+                                className="flex flex-row items-center gap-4"
+                            >   
+                                <FontAwesomeIcon icon={Icon} />
+                                <span className={!isOpen ? "block" : "hidden"}>{nav.title}</span>
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
     )
 }
