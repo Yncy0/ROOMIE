37f0755fe6e34fd0ff827b8a932b3b8c9cffc8a0
Yncy0 <paalayancy@gmail.com>
2024-10-09 14:19:14 +0800

    prototype: sidebar toggle

--- src/components/navigation/NavBarComponent.jsx
+++ src/components/navigation/NavBarComponent.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import { Navigation } from "./Navigation";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBarComponent() {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const openSideBar = () => {
+        setIsOpen(!isOpen)
+    }
+
     return(
-            <nav className="menu-container round-box p-6">
+            <div className="menu-container round-box p-6 gap-9 bg-white">
+                <div className="flex flex-row items-center gap-4">
+                    <button onClick={openSideBar}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                    <h1 className={`${isOpen ? "hidden" : "block"}`}>ROOMIE</h1>
+                </div>
                 <ul className="flex flex-col gap-12 pb-64">
                     {Navigation.map((nav) => {
                         const {Icon} = nav
@@ -15,14 +27,15 @@ export default function NavBarComponent() {
                                 <Link 
                                     to={`/${nav.title.toLowerCase()}`}
                                     onClick={() => setIsOpen((prev) => !prev)}
+                                    className="flex flex-row items-center gap-4"
                                 >   
                                     <FontAwesomeIcon icon={Icon} />
-                                    {/* <span>{nav.title}</span> */}
+                                    <span className={`${isOpen ? "hidden" : "block"}`}>{nav.title}</span>
                                 </Link>
                             </li>
                         )
                     })}
                 </ul>
-            </nav>
+            </div>
     )
 }
