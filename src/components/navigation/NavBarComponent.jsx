54e2381d767013cf7da8d797a1eac5b7393d5ad0
Yncy0 <paalayancy@gmail.com>
2024-10-10 16:57:22 +0800

    add: transition -> sidebar

--- src/components/navigation/NavBarComponent.jsx
+++ src/components/navigation/NavBarComponent.jsx
@@ -1,8 +1,8 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Navigation } from "./Navigation";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { ChevronFirst, ChevronLast } from "lucide-react";
 // import { SidebarContext } from "react-pro-sidebar";
 
 
@@ -14,22 +14,22 @@ export default function NavBarComponent() {
     }
 
     return(
-        <div className="menu-container round-box p-6 gap-9 bg-white">
-            <div className="flex flex-row items-center gap-4">
-                <button onClick={openSideBar}>
-                    <FontAwesomeIcon icon={faBars} />
+        <div className={`menu-container round-box p-6 gap-9 transition-all duration-300 ease-in-out ${isOpen ? 'w-20' : 'w-52'}`}>
+            <div className="flex flex-row items-center mb-7 gap-4">
+                <button onClick={openSideBar} className=" py-2">
+                    {isOpen ? <ChevronLast className="min-w-6"/> : <ChevronFirst className="min-w-6"/>}
                 </button>
                 <h1 className={`${!isOpen ? "block" : "hidden"} font-righteous text-xl`}>ROOMIE</h1>
             </div>
-            <ul className="flex flex-col gap-12 pb-64">
+            <ul className="flex flex-col gap-8 items-start">
                 {Navigation.map((nav) => {
                     const {Icon} = nav
                     return(
-                        <li key={nav.title}>
+                        <li key={nav.title} className="relative flex items-center pl-1">
                             <Link 
                                 to={`/${nav.title.toLowerCase()}`}
                                 // onClick={() => setIsOpen((prev) => !prev)}
-                                className="flex flex-row items-center gap-4"
+                                className="flex flex-row gap-4 items-center min-w-6"
                             >   
                                 <FontAwesomeIcon icon={Icon} />
                                 <span className={!isOpen ? "block" : "hidden"}>{nav.title}</span>
