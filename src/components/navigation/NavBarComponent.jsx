6c78ab518929e17370b5ba94758b0e3ee9d051d9
Yncy0 <paalayancy@gmail.com>
2024-10-12 13:44:25 +0800

    remove unnecesarry codes, fixed: items aligned at center

--- src/components/navigation/NavBarComponent.jsx
+++ src/components/navigation/NavBarComponent.jsx
@@ -16,25 +16,25 @@ export default function NavBarComponent() {
     //TO-DO: hover
     //TO-DO: icon on the header
     return(
-        <div className={`menu-container round-box p-14 gap-9 duration-300 ease-in-out ${isOpen ? 'w-20' : 'w-52'}`}>
-            <div className="flex flex-row items-center mb-7 gap-4">
+        <div className={`menu-container round-box p-14 gap-9 items-center duration-300 ease-in-out ${isOpen ? 'w-20' : 'w-52'}`}>
+            <div className="flex flex-row items-center mb-7 ">
                 <button onClick={openSideBar} className=" py-2">
                     {isOpen ? <ChevronLast className="min-w-6"/> : <ChevronFirst className="min-w-6"/>}
                 </button>
-                <h1 className={`duration-1000 ease-in-out ${!isOpen ? "block" : "hidden"} font-righteous text-xl`}>ROOMIE</h1>
+                <h1 className={`${!isOpen ? "block" : "hidden"} font-righteous text-xl`}>ROOMIE</h1>
             </div>
             <ul className="flex flex-col gap-8 items-start">
                 {Navigation.map((nav) => {
                     const {Icon} = nav
                     return(
-                        <li key={nav.title} className="relative flex items-center pl-1">
+                        <li key={nav.title} >
                             <Link 
                                 to={`/${nav.title.toLowerCase()}`}
                                 // onClick={() => setIsOpen((prev) => !prev)}
                                 className="flex flex-row gap-4 items-center min-w-6"
                             >   
                                 <FontAwesomeIcon icon={Icon} />
-                                <span className={`duration-1000 ease-in-out ${!isOpen ? "block" : "hidden"}`}>{nav.title}</span>
+                                <span className={` ${!isOpen ? "block" : "hidden"}`}>{nav.title}</span>
                             </Link>
                         </li>
                     )
