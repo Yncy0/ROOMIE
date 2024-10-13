3ffea6aadf880b43cc9e04cbbbd15b5e60af874d
Yncy0 <paalayancy@gmail.com>
2024-10-13 22:40:25 +0800

    little bit of design -> users page

---
+++ src/components/users/userDummy.jsx
@@ -0,0 +1,46 @@
+
+export const UserDuummy = [
+    {
+        name: "Ethan Cruz",
+        email: "ethancruz@email",
+        role: "User",
+        password: "1234****",
+        lastLogin: "28 Jan, 10:30 AM"
+    },
+    {
+        name: "Ethan Cruz",
+        email: "ethancruz@email",
+        role: "User",
+        password: "1234****",
+        lastLogin: "28 Jan, 10:30 AM"
+    },
+    {
+        name: "Ethan Cruz",
+        email: "ethancruz@email",
+        role: "User",
+        password: "1234****",
+        lastLogin: "28 Jan, 10:30 AM"
+    },
+    {
+        name: "Ethan Cruz",
+        email: "ethancruz@email",
+        role: "User",
+        password: "1234****",
+        lastLogin: "28 Jan, 10:30 AM"
+    },
+    {
+        name: "Ethan Cruz",
+        email: "ethancruz@email",
+        role: "User",
+        password: "1234****",
+        lastLogin: "28 Jan, 10:30 AM"
+    },
+    {
+        name: "Ethan Cruz",
+        email: "ethancruz@email",
+        role: "User",
+        password: "1234****",
+        lastLogin: "28 Jan, 10:30 AM"
+    },
+
+];
--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,10 +1,18 @@
+import React from "react";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { LucideFilter } from "lucide-react";
+import { UserDuummy } from "@/components/users/userDummy";
 
 export default function UsersPage() {
+    const [users, setUsers] = React.useState([]);
+
+    React.useEffect(() => {
+        setUsers([...UserDuummy])
+    }, [])
+
     return(
-        <div className="flex flex-col mx-20 mt-6">
+        <div className="flex flex-col mx-20 mt-6 gap-8">
             <div className="flex flex-row justify-between">
                 <div className="flex gap-4">
                     <FontAwesomeIcon icon={faMagnifyingGlass}/>
@@ -16,9 +24,30 @@ export default function UsersPage() {
                 </div>
                 <button className="bg-[#6EB229] text-white text-sm py-2 px-8 rounded-[50px]">Add User</button>
             </div>
-            {/* TO-DO: ADD HEADER */}
-            <div className="flex flex-col round-box gap-4">
-                
+            {/* TO-DO: FIX HEADER ALIGNENT*/}
+            <div className="flex flex-col round-box gap-4 p-6">
+                <ul className="flex flex-col gap-4">
+                <li className="flex flex-row justify-between gap-48">
+                        <h1>Name</h1>
+                        <h1>Email</h1>
+                        <h1>Role</h1>
+                        <h1>Password</h1>
+                        <h1>Last Login</h1>
+                        <h1>Edit User</h1>
+                        <h1>Delete User</h1>
+                    </li>
+                    {users.map((element, index) => (
+                        <li key={index} className="flex flex-row justify-between">
+                            <p>{element.name}</p>
+                            <p>{element.email}</p>
+                            <p>{element.role}</p>
+                            <p>{element.password}</p>
+                            <p>{element.lastLogin}</p>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
