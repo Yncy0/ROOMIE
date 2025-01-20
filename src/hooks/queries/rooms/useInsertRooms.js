6d2fd6e44fe47c658a7cc4e2e6ac482ab20a115b
Yncy <paalayancy@gmail.com>
2025-01-20 15:38:03 +0800

    added INSERT and UPDATE -> schedule

---
+++ src/hooks/queries/rooms/useInsertRooms.js
@@ -0,0 +1,18 @@
+import supabase from "@/utils/supabase";
+
+export const useInsertRooms = (i) => {
+  const query = async () => {
+    const { data, error } = await supabase.from("rooms").insert({
+      room_name: i.room_name,
+      room_image: i.room_image,
+      room_type: i.room_type,
+      room_capacity: i.room_capacity,
+    });
+
+    if (error) throw error;
+
+    return data;
+  };
+
+  return query;
+};
